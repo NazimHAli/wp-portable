19872bacf20d7f0676212ca3b02919c904294b10
Nazim <nazimali@gmail.com>
2021-11-06 10:00:35 -0400

    MNT: Delete extra files

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["standard-with-typescript"],
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  rules: {
-    "max-len": ["error", { code: 140 }],
-  },
-};
